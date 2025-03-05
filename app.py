8ffb0122a37d7ec1087409bd576f5546a3b8f4a1
Koushik V <varanasikoushikkumar@gmail.com>
2025-03-05 10:34:19 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 import time
 
 # Load API Key securely
-API_KEY = os.getenv("LF2I00FT4XHT2WE3")  # Set this in your environment variables
+API_KEY = os.getenv("HQE75GFESON26CBK")  # Set this in your environment variables
 
 # List of stock symbols
 companies = {
